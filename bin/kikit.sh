7f986e2a05d176aa3b523e0524ebaf1aa2f551e3
Yoshimasa Niwa <niw@niw.at>
2024-01-14 01:23:21 +0900

    Use patched version of KiKit.
    Use updated branch

--- bin/kikit.sh
+++ bin/kikit.sh
@@ -4,7 +4,7 @@ BIN_PATH=$(cd "$(dirname "$0")" >/dev/null; pwd)
 source "$BIN_PATH"/venv.sh
 
 if ! "$VENV_PATH"/bin/pip3 show -q kikit 2>/dev/null 1>&2; then
-  "$VENV_PATH"/bin/pip3 install kikit
+  "$VENV_PATH"/bin/pip3 install git+https://github.com/niw/KiKit.git@origin/v1.4.0_with_workaround
 fi
 
 exec "$VENV_PATH"/bin/kikit "$@"
