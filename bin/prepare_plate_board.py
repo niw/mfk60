1c1dea1960be060e61153f623b83e962fdbaece2
Yoshimasa Niwa <niw@niw.at>
2024-01-13 12:15:21 +0900

    Change script options.

--- bin/prepare_plate_board.py
+++ bin/prepare_plate_board.py
@@ -8,20 +8,20 @@ import pcbnew
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--board', type=str, required=True)
 parser.add_argument('-p', '--plate', type=str, required=True)
-parser.add_argument('--positioning-only', action='store_true')
+parser.add_argument('--update-board-shape', action='store_true')
+parser.add_argument('--update-position', action='store_true')
+parser.add_argument('--add-footprint', action='store_true')
 parser.add_argument('--footprint-pattern', default="^(Gateron_Low_Profile:.*|kikit:Tab|MountingHole.*:MountingHole.*)", type=str)
 args = parser.parse_args()
 
 board_path = args.board
 plate_path = args.plate
-positioning_only = args.positioning_only
-footprint_pattern = args.footprint_pattern
 
 board = pcbnew.LoadBoard(board_path)
 plate_board = pcbnew.LoadBoard(plate_path) if os.path.exists(plate_path) else pcbnew.CreateEmptyBoard()
 
 # PCB Shape
-if not positioning_only:
+if args.update_board_shape:
     for drawing in plate_board.GetDrawings():
         if drawing.GetClass() == "PCB_SHAPE":
             plate_board.RemoveNative(drawing)
@@ -42,26 +42,28 @@ jlc_text = find_text(board, jlc_string)
 if jlc_text:
     existing_jlc_text = find_text(plate_board, jlc_string)
     if existing_jlc_text is None:
-        if not positioning_only:
+        if args.add_footprint:
             plate_board.AddNative(jlc_text)
     else:
-        position = jlc_text.GetPosition()
-        existing_jlc_text.SetPosition(position)
+        if args.update_position:
+            position = jlc_text.GetPosition()
+            existing_jlc_text.SetPosition(position)
 
 # Footprints
 for footprint in board.GetFootprints():
     name = footprint.GetFPIDAsString()
-    if re.match(footprint_pattern, name):
+    if re.match(args.footprint_pattern, name):
         reference = footprint.GetReference()
         existing_footprint = plate_board.FindFootprintByReference(reference)
         if existing_footprint is None:
-            if not positioning_only:
+            if args.add_footprint:
                 plate_board.AddNative(footprint)
         else:
-            position = footprint.GetPosition()
-            existing_footprint.SetPosition(position)
-            orientation = footprint.GetOrientation()
-            existing_footprint.SetOrientation(orientation)
+            if args.update_position:
+                position = footprint.GetPosition()
+                existing_footprint.SetPosition(position)
+                orientation = footprint.GetOrientation()
+                existing_footprint.SetOrientation(orientation)
 
 pcbnew.Refresh()
 pcbnew.SaveBoard(plate_path, plate_board)
